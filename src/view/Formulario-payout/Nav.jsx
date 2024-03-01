51abc4cd17bdc6391d0f6023bd8aefcb4d8c9aa4
jean carlo <jean.quiroga@ci2.co>
2024-03-01 17:06:41 -0500

    casi se termina el menu de navegacion, faltan colocar logos y efectos hover

--- src/view/Formulario-payout/Nav.jsx
+++ src/view/Formulario-payout/Nav.jsx
@@ -3,33 +3,34 @@ export const Nav = () => {
   return (
     <section>
       <div className="contecInfo">
-        <div className="namePlan">logo</div>
         <div className="contecOptions">
-          <div>
-            <a href="/">
-              <img src="" alt="" /> <span>Cuenta</span>
-            </a>
-          </div>
-          <div>
-            <a href="/">
-              <img src="" alt="" /> <span>Empresas</span>
-            </a>
-          </div>
-          <div>
-            <a href="/">
-              <img src="" alt="" />
-              <span>Bolsillo</span>
-            </a>
+          <div className="namePlan">logo</div>
+          <div className="grupItem ">
+            <div className="item">
+              <a href="/">
+                <img src="" alt="" /><span>Cuenta</span>
+              </a>
+            </div>
+            <div className="item">
+              <a href="/">
+                <img src="" alt="" /><span>Empresas</span>
+              </a>
+            </div>
+            <div className="item">
+              <a href="/">
+                <img src="" alt="" /><span>Bolsillo</span>
+              </a>
+            </div>
           </div>
         </div>
         <div className="contecActionButtons">
-          <div>
+          <div className="ajustes">
             <a href="/">
               <img src="" alt="" />
               <span>Ajustes</span>
             </a>
           </div>
-          <div>
+          <div className="logup">
             <a href="/">
               <img src="" alt="" />
               <span>Cerrar sesión</span>
